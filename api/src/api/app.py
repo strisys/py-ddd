6e8592f6747fe939b0aeab369848a704f773de84
Stephen Trudel <github@strisys.com>
2025-06-08 23:27:16 +0000

    remove isort

--- api/src/api/app.py
+++ api/src/api/app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from services import CustomerDataAccessService
-from model import Customer  
+from services import CustomerDataAccessService, ProductDataAccessService
 
 app = FastAPI()
-
-customer_service = CustomerDataAccessService()
 
 @app.get("/customers")
 def get_customers() -> list[str]:
     return [c.name for c in CustomerDataAccessService().get()]
+
+@app.get("/products")
+def get_products() -> list[str]:
+    return [c.name for c in ProductDataAccessService().get()]
